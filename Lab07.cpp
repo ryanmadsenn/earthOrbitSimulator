869639750794048f8f1d5c42e50b77cc22ca57d0
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2023-02-17 14:28:32 -0700

    Fixed multiplier

--- Lab07.cpp
+++ Lab07.cpp
@@ -115,7 +115,7 @@ void callBack(const Interface* pUI, void* p)
     pDemo->gpsTrail.push_back(prev);
 
     // rotate the earth
-    double multiplier = 10;
+    double multiplier = 0.5;
 
     pDemo->angleEarth -= 0.00349 * multiplier;
     pDemo->angleShip -= 0.00349 * multiplier;

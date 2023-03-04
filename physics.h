a3b76dadcd9b2b977c8f47dbfcd75cc4da8d9647
bueze_nwokolo <nwokolobueze@gmail.com>
2023-03-04 11:27:07 -0700

    updated physics.h file

--- physics.h
+++ physics.h
@@ -1,13 +1,39 @@
 
 
 #pragma once
+#define _USE_MATH_DEFINES
+
+#include "velocity.h"
+#include "acceleration.h"
 
 class Physics 
 {
 public:
+	Physics() {}
 
+	double computeTimeDilation() { return hoursPerDay * minutesPerHour; }
 
+	double computeTimePerFrame() { return computeTimeDilation() * frameRate; }
+
+	double computeRotationSpeed() { return -(((2 * M_PI) / frameRate) * (computeTimeDilation() / seconsPerDay)); }
+
+	double computeSatHeight();
+
+	double computeAcceDueGravity() { return gravity * pow(earthRadius / (earthRadius + computeSatHeight()), 2); }
+
+	double computeGravityPullDirection();
+
+	double computeDistance();
+
+	void updatePosition();
 
 private:
-	Velocity velocity;
+	double time = 24;
+	double gravity = 9.80665;
+	double earthRadius = 6378000;
+	double frameRate = 30;
+	double hoursPerDay = 24;
+	double minutesPerHour = 60;
+	double secondsPerMinute = 60;
+	double seconsPerDay = hoursPerDay * minutesPerHour * secondsPerMinute;
 };
